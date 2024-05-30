0ede456db1a2e7ba58c9e2674740d0a431c5a908
Ádamo Nunes <adamo897@gmail.com>
2024-05-30 14:00:09 -0300

    Projeto Inicial

---
+++ src/main/java/com/suprevida/testecontratacao/TestecontratacaoApplication.java
@@ -0,0 +1,13 @@
+package com.suprevida.testecontratacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestecontratacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestecontratacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/suprevida/testecontratacao/TestecontratacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suprevida.testecontratacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestecontratacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
