072909799d5daa14a9cd45487cc2f1e3ad344798
Ádamo Nunes <adamo897@gmail.com>
2024-05-30 13:41:27 -0300

    Inicio projeto

---
+++ src/main/java/com/teste/suprevida/SuprevidaApplication.java
@@ -0,0 +1,12 @@
+package com.teste.suprevida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuprevidaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuprevidaApplication.class, args);
+	}
+}
